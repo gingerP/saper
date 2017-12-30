00de53057b29bbacb608014e4457c19eded61ad7
vinni <new.vinni@gmail.com>
2017-12-30 23:23:15 +0300

    - initial

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
     'use strict';
 
     var gameData = [];
@@ -9,6 +9,24 @@
         [-1, 0], [1, 0],
         [-1, 1], [0, 1], [1, 1]
     ];
+    var isGameFailed = false;
+
+    var flag = '\
+        <svg fill="red" height="15" viewBox="0 0 24 24" width="15" xmlns="http://www.w3.org/2000/svg">\
+        <path d="M0 0h24v24H0z" fill="none"/>\
+        <path d="M14.4 6L14 4H5v17h2v-7h5.6l.4 2h7V6z"/>\
+        </svg>';
+    var bomb = '\
+        <svg xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:cc="http://creativecommons.org/ns#" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:svg="http://www.w3.org/2000/svg" xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 15 15" enable-background="new 0 0 512 512" id="svg3773" width="15" height="15">\
+        <g id="g3771" transform="matrix(0.02929687,0,0,0.02929687,1.3197797e-4,0.00203714)">\
+        <path d="m 411.313,123.313 c 6.25,-6.25 6.25,-16.375 0,-22.625 -6.25,-6.25 -16.375,-6.25 -22.625,0 l -32,32 -9.375,9.375 -20.688,-20.688 c -12.484,-12.5 -32.766,-12.5 -45.25,0 l -16,16 c -1.261,1.261 -2.304,2.648 -3.31,4.051 C 240.326,132.865 216.741,128 192,128 86.133,128 0,214.133 0,320 c 0,105.867 86.133,192 192,192 105.867,0 192,-86.133 192,-192 0,-24.741 -4.864,-48.327 -13.426,-70.065 1.402,-1.007 2.79,-2.049 4.051,-3.31 l 16,-16 c 12.5,-12.492 12.5,-32.758 0,-45.25 l -20.688,-20.688 9.375,-9.375 z M 192,224 c -52.938,0 -96,43.063 -96,96 0,8.836 -7.164,16 -16,16 -8.836,0 -16,-7.164 -16,-16 0,-70.578 57.422,-128 128,-128 8.836,0 16,7.164 16,16 0,8.836 -7.164,16 -16,16 z" id="path3759" inkscape:connector-curvature="0"/>\
+        <path d="m 459.02,148.98 c -6.25,-6.25 -16.375,-6.25 -22.625,0 -6.25,6.25 -6.25,16.375 0,22.625 l 16,16 c 3.125,3.125 7.219,4.688 11.313,4.688 4.094,0 8.188,-1.563 11.313,-4.688 6.25,-6.25 6.25,-16.375 0,-22.625 z" id="path3761"/>\
+        <path d="m 340.395,75.605 c 3.125,3.125 7.219,4.688 11.313,4.688 4.094,0 8.188,-1.563 11.313,-4.688 6.25,-6.25 6.25,-16.375 0,-22.625 l -16,-16 c -6.25,-6.25 -16.375,-6.25 -22.625,0 -6.25,6.25 -6.25,16.375 0,22.625 z" id="path3763"/>\
+        <path d="m 400,64 c 8.844,0 16,-7.164 16,-16 V 16 C 416,7.164 408.844,0 400,0 391.156,0 384,7.164 384,16 v 32 c 0,8.836 7.156,16 16,16 z" id="path3765"/>\
+        <path d="m 496,96.586 h -32 c -8.844,0 -16,7.164 -16,16 0,8.836 7.156,16 16,16 h 32 c 8.844,0 16,-7.164 16,-16 0,-8.836 -7.156,-16 -16,-16 z" id="path3767"/>\
+        <path d="m 436.98,75.605 c 3.125,3.125 7.219,4.688 11.313,4.688 4.094,0 8.188,-1.563 11.313,-4.688 l 32,-32 c 6.25,-6.25 6.25,-16.375 0,-22.625 -6.25,-6.25 -16.375,-6.25 -22.625,0 l -32,32 c -6.251,6.25 -6.251,16.375 -10e-4,22.625 z" id="path3769"/>\
+        </g>\
+        </svg>';
 
     function renderGamePane(data) {
         var xIndex = 0;
@@ -16,19 +34,19 @@
         var row = '';
         var item;
         var td = '';
-        var table = '<table style="width: ' + (data.length * 20)+ 'px"><colgroup>';
-        for(;xIndex < data.length; xIndex++) {
+        var table = '<table style="width: ' + (data.length * 20) + 'px"><colgroup>';
+        for (; xIndex < data.length; xIndex++) {
             table += '<col width="20px"/>';
         }
         table += '</colgroup>';
-        for(xIndex = 0;xIndex < data.length; xIndex++) {
+        for (xIndex = 0; xIndex < data.length; xIndex++) {
             yIndex = 0;
             row = '<tr>';
-            for(;yIndex < data[xIndex].length; yIndex++) {
+            for (; yIndex < data[xIndex].length; yIndex++) {
                 item = data[xIndex][yIndex];
                 row += '<td id="cell-' + xIndex + '-' + yIndex + '" class="game-cell cell-closed"></td>';
             }
-            row +='</tr>';
+            row += '</tr>';
             table += row;
         }
         table += '</table>';
@@ -42,20 +60,21 @@
         var cell;
         var data = [];
 
-        for(xIndex = 0;xIndex < width; xIndex++) {
+        for (xIndex = 0; xIndex < width; xIndex++) {
             row = [];
-            for(yIndex = 0;yIndex < height; yIndex++) {
+            for (yIndex = 0; yIndex < height; yIndex++) {
                 var isBomb = Boolean(Math.round(Math.random() - 0.3));
                 row.push({
                     isBomb: isBomb,
+                    isOpened: false,
                     value: 0
                 });
             }
             data.push(row);
         }
 
-        for(xIndex = 0;xIndex < width; xIndex++) {
-            for(yIndex = 0;yIndex < height; yIndex++) {
+        for (xIndex = 0; xIndex < width; xIndex++) {
+            for (yIndex = 0; yIndex < height; yIndex++) {
                 cell = data[xIndex][yIndex];
                 if (!cell.isBomb) {
                     cell.value = getBombsCountAround(data, xIndex, yIndex, width, height);
@@ -69,7 +88,7 @@
         var shiftIndex = 0;
         var result = 0;
         var shift;
-        for(;shiftIndex < shifts.length; shiftIndex++) {
+        for (; shiftIndex < shifts.length; shiftIndex++) {
             shift = shifts[shiftIndex];
             if (xIndex + shift[0] >= 0 && xIndex + shift[0] < maxX
                 && yIndex + shift[1] >= 0 && yIndex + shift[1] < maxY
@@ -87,7 +106,7 @@
         var shift;
         var maxX = data.length;
         var maxY = data[0].length;
-        for(;shiftIndex < shifts.length; shiftIndex++) {
+        for (; shiftIndex < shifts.length; shiftIndex++) {
             shift = shifts[shiftIndex];
             var x = xIndex + shift[0];
             var y = yIndex + shift[1];
@@ -112,26 +131,41 @@
 
     function initEvents(data) {
         var pane = document.getElementById('saper-game-pane');
-        pane.addEventListener('click', function(event) {
-            if (event.target.tagName === 'TD' && event.target.className.indexOf('cell-open') < 0) {
+        var reloadButton = document.getElementById('saper-toolbar-reload');
+        pane.addEventListener('click', function (event) {
+            if (event.target.tagName === 'TD' && !hasClass(event.target, 'cell-open')) {
                 var match = /cell-(\d*)-(\d*)/g.exec(event.target.id);
                 var pos = [+match[1], +match[2]];
-                openCell(data, pos[0], pos[1], true);
+                openCell(data, pos[0], pos[1], true, true);
             }
-        })
+        });
+        pane.addEventListener('contextmenu', function (event) {
+            if (event.target.tagName === 'TD') {
+                setFlag(event.target);
+                event.preventDefault();
+            }
+        });
+        reloadButton.addEventListener('click', function() {
+           if (!isGameFailed && confirm('Are you sure you want to start over?') || isGameFailed) {
+               reload();
+           }
+        });
     }
 
-    function openCell(data, x, y, shouldOpenAround) {
+    function openCell(data, x, y, shouldOpenAround, isFirstBomb) {
         var item = data[x][y];
         var cell = document.getElementById('cell-' + x + '-' + y);
-        cell.className = cell.className.replace(/( |^)cell-closed( |$)/g, ' ');
-        if (!/( |^)cell-opened( |$)/g.test(cell.className)) {
-            cell.className += ' cell-opened';
-        }
+        removeClass(cell, 'cell-closed');
+        addClass(cell, 'cell-opened');
+        item.isOpened = true;
         if (item && item.isBomb) {
-            cell.innerHTML = 'x';
+            isGameFailed = true;
+            openBomb(data, x, y, cell, isFirstBomb);
         } else if (item && !item.isBomb) {
-            cell.innerHTML = item.value;
+            if (item.value !== 0) {
+                cell.innerHTML = item.value;
+                addClass(cell, 'cell-value-' + item.value);
+            }
             if (shouldOpenAround && item.value === 0) {
                 var blankLake = getBlankLake(data, x, y);
                 openLake(blankLake, data);
@@ -139,8 +173,24 @@
         }
     }
 
+    function addClass(target, className) {
+        var regexp = new RegExp('( |^)' + className + '( |$)', 'g');
+        if (!regexp.test(target.className)) {
+            target.className += ' ' + className;
+        }
+    }
+
+    function removeClass(target, className) {
+        var regexp = new RegExp('( |^)' + className + '( |$)', 'g');
+        target.className = target.className.replace(regexp, ' ');
+    }
+
+    function hasClass(target, className) {
+        return target.className.indexOf(className) >= 0;
+    }
+
     function openLake(items, data) {
-        for(var index = 0; index < items.length; index++) {
+        for (var index = 0; index < items.length; index++) {
             var item = items[index];
             openCell(data, item[0], item[1], false);
         }
@@ -152,7 +202,7 @@
         var iterations = [[x, y]];
         var index = 0;
         var cell;
-        while(true) {
+        while (true) {
             cell = iterations[index];
             if (!cell) {
                 break;
@@ -171,15 +221,46 @@
         return result;
     }
 
+    function openBomb(data, x, y, target, isFirst) {
+        target.innerHTML = bomb;
+        if (isFirst) {
+            addClass(target, 'cell-first-bomb');
+            openPane(data);
+        }
+    }
+
     function hashFromList(list) {
         var result = [];
-        for(var index = 0; index < list.length; index++) {
+        for (var index = 0; index < list.length; index++) {
             result.push(list[index][0] + '-' + list[index][1]);
         }
         return result;
     }
 
-    gameData = initializeData(xSize, ySize);
-    renderGamePane(gameData);
+    function setFlag(target) {
+        target.innerHTML = flag;
+    }
+
+    function openPane(data) {
+        for(var xIndex = 0; xIndex < data.length; xIndex++) {
+            for(var yIndex = 0; yIndex < data[xIndex].length; yIndex++) {
+                (function() {
+                    var x = xIndex;
+                    var y = yIndex;
+                    setTimeout(function() {
+                        openCell(data, x, y, false, false);
+                    }, 0);
+                })();
+            }
+        }
+    }
+
+    function reload() {
+        isGameFailed = false;
+        gameData = initializeData(xSize, ySize);
+        renderGamePane(gameData);
+    }
+
+    reload();
     initEvents(gameData);
 })();
